dd970c0b17ae2c651a284eec99c17a6fcc6a59f8
Natthanon <nuttanon172@gmail.com>
2023-10-08 09:52:44 +0000

    Update Signal

--- 04_utils.c
+++ 04_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   04_utils.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ntairatt <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ntairatt <ntairatt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 18:27:14 by lbhuprad          #+#    #+#             */
-/*   Updated: 2023/10/07 11:06:32 by ntairatt         ###   ########.fr       */
+/*   Updated: 2023/10/08 09:51:09 by ntairatt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,11 @@
 
 void	sig_handler(int sig_number)
 {
-	(void)sig_number;
-	printf("\nCurrent directory: %s\n", getcwd(NULL, 0));
-	rl_on_new_line();
-	rl_redisplay();
-	signal(SIGINT, sig_handler);
-	return ;
-}
-
-void	sig_handler1(int sig_number)
-{
-	(void)sig_number;
-	printf("in siguser\n");
-	exit(0);
-	return ;
+	if (sig_number == SIGINT)
+	{
+		printf("\nCurrent directory: %s\n", getcwd(NULL, 0));
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
 }
